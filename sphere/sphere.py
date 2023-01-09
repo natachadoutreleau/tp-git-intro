01468b76788b333059435fd2cbfa1bb11322828b
Natacha Doutreleau <natacha.doutreleau@gmail.com>
2023-01-09 15:28:30 +0100

    decommenté step 3

--- sphere/sphere.py
+++ sphere/sphere.py
@@ -14,9 +14,9 @@ class Sphere(object):
     #    return '%s(%s)' % (self.__class__.__name__, self.radius)
 
     def surface(self):
-        # *** STEP 3 ***
-        # uncomment the following line
-        #return 4.0 * 3.1416 * self.radius ** 2
+         *** STEP 3 ***
+         uncomment the following line
+        return 4.0 * 3.1416 * self.radius ** 2
         pass
 
     def volume(self):
